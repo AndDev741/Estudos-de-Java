e5b35e3823b7f83c5a82a9966dcf0c84ea321698
AndDev <anddesenvolved@gmail.com>
2024-03-12 15:04:12 -0300

    Type converter

---
+++ 5-Converter/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int idade = 30;
+        String valor = Integer.toString(idade);
+
+        String pontos = "100";
+        int pontosInt = Integer.parseInt(pontos);
+
+
+        System.out.println(valor);
+        System.out.println(pontosInt);
+    }
+}
